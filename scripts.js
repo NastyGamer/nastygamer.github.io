b0354d10a2a52f513ca0bdae4963b8eabb9d5f58
NastyGamer <32023338+NastyGamer@users.noreply.github.com>
2021-02-05 18:03:10 +0100

    Add files via upload

---
+++ scripts.js
@@ -0,0 +1,19 @@
+function openDoc(url) {
+    window.open("docs/" + url + "/index.html", '_blank');
+}
+
+function openGithub(url) {
+    window.open("https://github.com/NastyGamer/" + url, '_blank');
+}
+
+function github() {
+    window.open('http://github.com/NastyGamer', '_blank');
+}
+
+function twitter() {
+    window.open('https://twitter.com/nastygamer123', '_blank');
+}
+
+function bitbucket() {
+    window.open('https://bitbucket.org/NastyGamer/', '_blank');
+}
